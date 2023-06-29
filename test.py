d61b6ba731223274b4e869453c78f09003f20ec6
nekocat0320 <e2013102@st.t-kougei.ac.jp>
2023-06-29 18:22:58 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,119 @@
+import datetime,glob,os,gc,sys,time
+from importlib import import_module
+
+# どこから起動してもいいように、ソースファイルの場所に移動している。
+os.chdir(f"{__file__[:-7]}")
+
+# 評価対象をまとめて読み込む
+method_dic = {}
+error_array = []
+dir_list = glob.glob("target/*/")
+print(f"以下のフォルダを検出しました。\n{dir_list}\n")
+for dir_name in dir_list:
+    try:
+        # 評価対象をモジュールとして読み込む。
+        sys.path.append(f"{__file__[:-7]}target\\{dir_name[7:-1]}")
+        #method_dic[dir_name[7:-1]] = import_module("main")
+        method_dic[dir_name[7:-1]] = import_module(f"target.{dir_name[7:-1]}.main")
+        sys.path.remove(f"{__file__[:-7]}target\\{dir_name[7:-1]}")
+    except:
+        error_array.append(dir_name[7:-1])
+target_list = list(method_dic.keys())
+# 読み込みエラーが一度でも起きた場合に警告を表示
+if len(error_array) > 0:
+    print("Warning: 以下のフォルダからシステムが読み込めませんでした。")
+    print(error_array)
+    print('フォルダ名やファイル名に "." が含まれていると、正常に読み込まれないことがあります。\n読み込む必要のないフォルダである場合は無視し、計測したい場合はフォルダ名を変更してください。\n')
+
+def main() -> None:
+    max_count = 10 # 計測回数の指定
+    diff_dic = {} # 計測結果まとめの辞書
+    res_dic = {} # 出力結果まとめの辞書
+    writeLinesArray = [] # 出力するファイルの中身
+
+    # モジュールをひとつづつ選んで計測していく
+    for target_name in target_list:
+        print(f"{target_name} 計測を開始します。")
+        response = [] # 計測結果の配列
+        ex_res = [] # 出力結果の配列
+
+        # 計測
+        for i in range(max_count):
+            print(str(i + 1) + "回目",end = " ",flush = True)
+            # 処理時間と出力結果を受け取り配列に格納。何もない場合でも return_object には None (多言語における null 相当) が入る。
+            diff,return_object = func(target_name)
+            response.append(str(diff.seconds + diff.microseconds / 1000000))        
+            ex_res.append(return_object)
+            # ここまで
+            print("終了")
+        print("計測が終了しました。\n")
+
+        diff_dic[target_name] = response # 名前をキーに、結果を値に
+
+        # 出力結果を表示に加える
+        res_dic[target_name] = ex_res # 名前をキーに、結果を値に
+        
+    # 計測結果のテキスト化
+    writeLinesArray.append(f'num,{",".join(list(diff_dic.keys()))}\n')
+    for i in range(max_count):
+        writeLinesArray.append(f"{i + 1},")
+        for target_name in list(diff_dic.keys()):
+            writeLinesArray[-1] += diff_dic[target_name][i]
+            if target_name != list(diff_dic.keys())[-1]: # 最後の対象以外の時は、後ろに,を付けて切り分ける
+                writeLinesArray[-1] += ","
+            else:
+                writeLinesArray[-1] += "\n"
+    
+    # 出力結果のテキスト化
+    for res in list(res_dic.keys()):
+        # 出力がいずれか一か所でも出ていた場合はテキスト化を行い、そうでなければ行わない。
+        if object_check(res_dic[res]):
+            writeLinesArray.append(f"\nresponse\n")
+            for i in range(max_count):
+                writeLinesArray.append(f"{i + 1},")
+                for target_name in list(res_dic.keys()):
+                    writeLinesArray[-1] += f"{res_dic[target_name][i]}".replace("\n","  ").replace(","," ") # 出力結果をcsvに出力する際に形が崩れるのを防ぐため、改行コードとカンマを排除している。
+                    if target_name != list(res_dic.keys())[-1]: # 最後の対象以外の時は、後ろに,を付けて切り分ける
+                        writeLinesArray[-1] += ","
+                    else:
+                        writeLinesArray[-1] += "\n"
+            break
+        else:
+            pass
+
+    # csvとして書き出し
+    now = datetime.datetime.now().strftime(r"%Y-%m-%d_%H:%M:%S")
+    file_name = now.replace(":","-") + "_diff.csv" # ファイル名に組み込めないコロンを排除
+    print("CSVファイルを出力しています...",end = " ",flush = True)
+    with open(f"{__file__[:-7]}\\diff\\{file_name}",mode="w") as f:
+        f.write(f"{now.replace('-','/').replace('_',' ')}\n") # 最初の行にファイル出力のタイミングを記載、個人的にわかりやすい表記に変えている
+        f.writelines(writeLinesArray) # データの表を書き込み
+    print("終了しました。\n")
+
+def func(target_name: str) -> datetime.timedelta:
+    # target_nameの内容に応じて処理が変わるようにしておく
+    while True:
+        try:
+            gc.disable() # ガベージコレクションによるメモリ開放が処理に与える影響を排除している。これを含みたい場合はコメントアウトしてもいい。
+            start = datetime.datetime.now()
+            return_object = method_dic[target_name].test() # 出力または None
+            end = datetime.datetime.now()
+            gc.enable()
+            diff = end - start # 処理時間
+            break
+        except Exception as e:
+            gc.enable()
+            print(e)
+            continue
+    return diff,return_object
+
+# リストに None 以外が含まれていれば True 、なければ False を返す。
+def object_check(array: list) -> bool:
+    for i in array:
+        if i is not None:
+            return True
+    return False
+
+if __name__ == "__main__":
+    main()
+    input("Enterで終了します。")
